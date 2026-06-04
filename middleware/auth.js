85625cc034d9d8943435268ebdbd30fecc6d42b8
tochukwu <okeakputochukwu9@gmail.com>
2026-06-04 13:27:36 -0400

    auth middleware added

---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+export const authenticate = async (req, res, next) => {
+  const access_token = req.cookies.access_token;
+
+  if (access_token) {
+    return res.status(403).json({
+      success: false,
+      message: "no tokon provided",
+    });
+  }
+  try {
+    const decoded = jwt.verify(access_token, process.env.ACCESS_TOKEN);
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        success: false,
+        message: "token expired",
+      });
+    }
+
+    return res.status(401).json({
+      success: false,
+      message: "invalid token",
+    });
+  }
+};
