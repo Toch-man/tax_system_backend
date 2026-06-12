5dc655abcf79a3b54b4b3ff597d7d135a243f458
tochukwu <okeakputochukwu9@gmail.com>
2026-06-12 15:08:20 -0400

    mongobd connect rsolved

--- config/database.js
+++ config/database.js
@@ -1,11 +1,17 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    if (!process.env.LIVE_URL) {
+      throw new Error("Missing MONGO connection string");
+    }
+
     await mongoose.connect(process.env.LIVE_URL);
-    console.log('Database connected successfully')
+
+    console.log("Database connected successfully");
   } catch (error) {
-    console.error('Database connection failed' , error);
+    console.error("Database connection failed:", error.message);
+    process.exit(1); // stop server if DB fails
   }
 };
 
